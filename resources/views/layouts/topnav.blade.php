bac4676b1f004d44f7b29c1f1ce66e0370739337
Matteo Giri <s1090263@studenti.univpm.it>
2021-05-19 18:42:22 +0200

    aggiornata top nav

--- resources/views/layouts/topnav.blade.php
+++ resources/views/layouts/topnav.blade.php
@@ -1,6 +1,6 @@
 
 <a href="{{ route('frontpage') }}"  id="home" class="{{ (Route::currentRouteName() == 'frontpage') ? 'active' : '' }}">Home</a>
-<a href="{{ route('catalog') }}" id="catalogo" class="{{ (Route::currentRouteName() == 'catalog') ? 'active' : '' }}">Catalogo</a>
+<a href="{{ route('catalog') }}" id="catalogo" class="{{ (Route::currentRouteName() == 'catalog' || Route::currentRouteName() == 'Ricerca') ? 'active' : '' }}">Catalogo</a>
 			
 			<a href="javascript:void(0);" id = "icon" class="icon" onclick ="myFunction()">
 				<i class="fa fa-bars"></i>
