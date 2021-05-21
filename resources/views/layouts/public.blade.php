10f3721a4d7332781ebbdeee1b004e1541947109
Edoardo Bilancia <s1087315@studenti.univpm.it>
2021-05-21 10:58:29 +0200

    tel link e favicon

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <link rel="stylesheet" type="text/css" href="{{ asset('tWeb.css') }}" >
         <link rel="stylesheet" type = "text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="icon" type="image/png" href="{{ asset('images/favicon.png')}}">
         <title>ProgTweb | @yield('title', 'Catalogo')</title>
         <script>
          function myFunction (){
