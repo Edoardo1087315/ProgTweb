4ff6fc93258dbfb9938262a3cd19975960235b3b
Edoardo Bilancia <s1087315@studenti.univpm.it>
2021-05-27 12:46:20 +0200

    fixareadmin

--- resources/views/Area_Admin.blade.php
+++ resources/views/Area_Admin.blade.php
@@ -83,7 +83,7 @@ $(function() {
                   $(this).addClass('fa fa-send');
                   button.not($(this)).hide();
                   $('.buttons').hide();
-                  $('#aannulla').showw();
+                  $('#aannulla').show();
                   
                   }
               })
