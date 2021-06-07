9fbcfc5931a9d1cac62f4833c78707492bb34947
Edoardo Bilancia <s1087315@studenti.univpm.it>
2021-06-07 16:20:59 +0200

    modifiche per company details

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -11,61 +11,78 @@ use App\Http\Requests\NewFaqRequest;
 use App\Http\Requests\UpdateFaqRequest;
 use App\Http\Requests\DeleteFaqRequest;
 
-Class AdminController extends Controller{
+Class AdminController extends Controller {
 
-    protected $_applicationPublic;
-    protected $_applicationAdmin;
-    
-    public function __construct() {
-        $this->middleware('can:isAdmin');
-        $this->_applicationPublic = new application_public;
-        $this->_applicationAdmin = new application_admin;
-    }
-         
-    public function showAreaAdmin(){
-       $users = $this->_applicationAdmin->getUsers();
-        return view('Area_admin')->with('users',$users);
-    }
-    
-        public function deleteUser(DeleteUserRequest $request){
-        $this->_applicationAdmin->deleteTicketsById($request->userid);
-        $this->_applicationAdmin->deleteUserById($request->userid);
-        return redirect('AreaAmministratore');
-        }
-        
-        public function newCompanyRequest(NewCompanyRequest $request) {
-        $this->_applicationAdmin->addCompany($request);
-            return response()->json(['redirect' => route('Area_Admin')]);
-        }
-        
-        public function updateCompanyRequest(UpdateCompanyRequest $request){
-             $this->_applicationAdmin->updateCompany($request);
-            return response()->json(['redirect' => route('Area_Admin')]);
-        }
-            
-        public function getCompanyToUpdate($id) {
-            $selected_company = $this->_applicationAdmin->getUserById($id);
-            $users = $this->_applicationAdmin->getUsers();
-        return view('Area_Admin')->with('selected_company', $selected_company)->with('users', $users);
-        }
-    
-    public function getFaqToDelete(DeleteFaqRequest $request){
-        $this->_applicationAdmin->deleteFaqById($request->faqId);
-        return redirect('Faq');
-    }
-    
-    public function getFaqToUpdate(UpdateFaqRequest $request){
-        $this->_applicationAdmin->updateFaq($request);
-        return response()->json(['redirect' => route('Faq')]);
-    }
-    
-    public function newFaqRequest(NewFaqRequest $request) {
-             $this->_applicationAdmin->addFaq($request);
-            return response()->json(['redirect' => route('Faq')]);
-        }
-    public function showAdminFaq(){
-        $Faq = $this->_applicationPublic->getFaq();
-        return view('Faq_Admin')->with('_faq',$Faq);
-    }
-    
+ protected $_applicationPublic;
+protected $_applicationAdmin;
+
+ public function __construct() {
+$this->middleware('can:isAdmin');
+$this->_applicationPublic = new application_public;
+$this->_applicationAdmin = new application_admin;
+}
+
+ public function showAreaAdmin() {
+$users = $this->_applicationAdmin->getUsers();
+$companies = $this->_applicationAdmin->getCompanies();
+$companiesWithAnalisi = [];
+foreach ($companies as $company)
+$companiesWithAnalisi[$company->id] = ["company" => $company, 'incasso' => $this->_applicationAdmin->getCompanyEventsIncasso($company->id), 'venduti' => $this->_applicationAdmin->getCompanyEventsVenduti($company->id)];
+
+ return view('Area_admin')->with('users', $users)->with('companies', $companies)->with('companiesWithAnalisi', $companiesWithAnalisi);
+}
+
+/*
+public function showAreaAdmin(){
+$users = $this->_applicationAdmin->getUsers();
+return view('Area_admin')->with('users',$users);
+} */
+
+ public function deleteUser(DeleteUserRequest $request) {
+$this->_applicationAdmin->deleteTicketsById($request->userid);
+$this->_applicationAdmin->deleteUserById($request->userid);
+return redirect('AreaAmministratore');
+}
+
+ public function newCompanyRequest(NewCompanyRequest $request) {
+$this->_applicationAdmin->addCompany($request);
+return response()->json(['redirect' => route('Area_Admin')]);
+}
+
+ public function updateCompanyRequest(UpdateCompanyRequest $request) {
+$this->_applicationAdmin->updateCompany($request);
+return response()->json(['redirect' => route('Area_Admin')]);
+}
+
+ public function getCompanyToUpdate($id) {
+$selected_company = $this->_applicationAdmin->getUserById($id);
+$users = $this->_applicationAdmin->getUsers();
+$companies = $this->_applicationAdmin->getCompanies();
+$companiesWithAnalisi = [];
+foreach ($companies as $company)
+$companiesWithAnalisi[$company->id] = ["company" => $company, 'incasso' => $this->_applicationAdmin->getCompanyEventsIncasso($company->id), 'venduti' => $this->_applicationAdmin->getCompanyEventsVenduti($company->id)];
+
+ return view('Area_Admin')->with('selected_company', $selected_company)->with('users', $users)->with('companiesWithAnalisi', $companiesWithAnalisi);
+}
+
+ public function getFaqToDelete(DeleteFaqRequest $request) {
+$this->_applicationAdmin->deleteFaqById($request->faqId);
+return redirect('Faq');
+}
+
+ public function getFaqToUpdate(UpdateFaqRequest $request) {
+$this->_applicationAdmin->updateFaq($request);
+return response()->json(['redirect' => route('Faq')]);
+}
+
+ public function newFaqRequest(NewFaqRequest $request) {
+$this->_applicationAdmin->addFaq($request);
+return response()->json(['redirect' => route('Faq')]);
+}
+
+ public function showAdminFaq() {
+$Faq = $this->_applicationPublic->getFaq();
+return view('Faq_Admin')->with('_faq', $Faq);
+}
+
 }
--- app/Models/application_admin.php
+++ app/Models/application_admin.php
@@ -1,10 +1,10 @@
 <?php
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 
 namespace App\Models;
 use App\Models\Resources\Ticket;
@@ -14,62 +14,70 @@ use App\Models\Resources\Faqs;
 use Illuminate\Support\Facades\Hash;
 
 
-/**
- * Description of application_admin
- *
- * @author lorti
- */
 class application_admin {
-    
-    public function getUsers(){
-        return User::all();
-    }
-    
-    public function getUserById($userid){
-        return User::where('id',$userid)->first();
-    }
-    
-    public function deleteUserById($userid){
-        Event::where('societaid',$userid)->delete();
-        User::where('id',$userid)->delete();
-    }
-    
-    public function deleteTicketsById($userId){
-        Ticket::where('user_id',$userId)->delete();
-    }
-    
-    public function addCompany($company){
-         $user = new User;
-         $user->fill($company->validated());
-         $user->role = 'company';
-         $user->password = Hash::make($company->password);
-         $user->save();
-    }
-    
-    public function updateCompany($companyAttrs){
-        $user = User::find($companyAttrs->companyid);
-        $user->nome = $companyAttrs->nome;
-        $user->data_nascita = $companyAttrs->data_nascita;
-        $user->sitoweb = $companyAttrs->sitoweb;
-        $user->email = $companyAttrs->email;
-        $user->telefono = $companyAttrs->telefono;
-        $user->save();
-    }
-    
-     public function addFaq($faqToAdd){
-        $faq = new Faqs;
-        $faq->fill($faqToAdd->validated());
-        $faq->save();
-    }
-    
-    public function updateFaq($faqToUpdate){
-        $faq = Faqs::find($faqToUpdate->faqId);
-        $faq->Domanda = $faqToUpdate->Domanda;
-        $faq->Risposta = $faqToUpdate->Risposta;
-        $faq->save();
-    }
-     public function deleteFaqById($faqID){
-        Faqs::where('faqId',$faqID)->delete();
-    }
+/* public function getUsers(){
+return User::all();
+}*/
+public function getUserById($userid){
+return User::where('id',$userid)->first();
 }
-
+public function getUsers(){
+return User::where('role','user')->get();
+}
+public function getCompanies(){
+return User::where('role','company')->get();
+}
+public function getCompanyEventsIncasso($idsocieta) {
+$events = Event::where('societaid', $idsocieta)->join('users','event.societaid','=','users.id')->
+get(['event.*','users.nome as societa']);
+$incassoTot = 0;
+foreach($events as $event)
+$incassoTot += $event->incassoTotale;
+return $incassoTot;
+}
+public function getCompanyEventsVenduti($idsocieta) {
+$events = Event::where('societaid', $idsocieta)->join('users','event.societaid','=','users.id')->
+get(['event.*','users.nome as societa']);
+$biglVenduti = 0;
+foreach($events as $event)
+$biglVenduti += $event->bigl_acquis;
+return $biglVenduti;
+}
+public function deleteUserById($userid){
+Event::where('societaid',$userid)->delete();
+User::where('id',$userid)->delete();
+}
+public function deleteTicketsById($userId){
+Ticket::where('user_id',$userId)->delete();
+}
+public function addCompany($company){
+$user = new User;
+$user->fill($company->validated());
+$user->role = 'company';
+$user->password = Hash::make($company->password);
+$user->save();
+}
+public function updateCompany($companyAttrs){
+$user = User::find($companyAttrs->companyid);
+$user->nome = $companyAttrs->nome;
+$user->data_nascita = $companyAttrs->data_nascita;
+$user->sitoweb = $companyAttrs->sitoweb;
+$user->email = $companyAttrs->email;
+$user->telefono = $companyAttrs->telefono;
+$user->save();
+}
+public function addFaq($faqToAdd){
+$faq = new Faqs;
+$faq->fill($faqToAdd->validated());
+$faq->save();
+}
+public function updateFaq($faqToUpdate){
+$faq = Faqs::find($faqToUpdate->faqId);
+$faq->Domanda = $faqToUpdate->Domanda;
+$faq->Risposta = $faqToUpdate->Risposta;
+$faq->save();
+}
+public function deleteFaqById($faqID){
+Faqs::where('faqId',$faqID)->delete();
+}
+}
